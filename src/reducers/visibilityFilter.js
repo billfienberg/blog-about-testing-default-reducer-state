c3b4008b887854d5614317145333319e57b061eb
Bill Fienberg <billfienberg@users.noreply.github.com>
2018-04-17 00:28:22 -0500

    Sync default visibilityFilter state between the reducer and test

--- src/reducers/visibilityFilter.js
+++ src/reducers/visibilityFilter.js
@@ -1,5 +1,6 @@
 import { SET_VISIBILITY_FILTER, VisibilityFilters } from "../actions";
 const { SHOW_ALL, SHOW_COMPLETED } = VisibilityFilters;
+export const defaultState = SHOW_ALL;
 
 function visibilityFilter(state = SHOW_ALL, action) {
   switch (action.type) {
--- src/reducers/visibilityFilter.test.js
+++ src/reducers/visibilityFilter.test.js
@@ -1,11 +1,10 @@
-import reducer from "./visibilityFilter";
-import { VisibilityFilters } from "../actions";
+import reducer, { defaultState } from "./visibilityFilter";
 
 describe(`the visibilityFilter reducer`, () => {
   it(`should return the default state`, () => {
     const state = undefined;
     const action = {};
-    const expectedOutput = VisibilityFilters.SHOW_ALL;
+    const expectedOutput = defaultState;
     expect(reducer(state, action)).toEqual(expectedOutput);
   });
 });
