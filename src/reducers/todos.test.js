17bb887f5218ff5bc748d938d93451a71a78e2ef
Bill Fienberg <billfienberg@users.noreply.github.com>
2018-04-17 00:18:24 -0500

    Fix todos reducer test that started failing after the default todos state was changed

--- src/reducers/todos.test.js
+++ src/reducers/todos.test.js
@@ -4,7 +4,16 @@ describe(`the todos reducer`, () => {
   it(`should return the default state`, () => {
     const state = undefined;
     const action = {};
-    const expectedOutput = [];
+    const expectedOutput = [
+      {
+        text: "Consider using Redux",
+        completed: true
+      },
+      {
+        text: "Keep all state in a single tree",
+        completed: false
+      }
+    ];
     expect(reducer(state, action)).toEqual(expectedOutput);
   });
 });
