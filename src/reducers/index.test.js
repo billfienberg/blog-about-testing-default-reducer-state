b47eada226f133b3e86880dff1677999fdd47d69
Bill Fienberg <billfienberg@users.noreply.github.com>
2018-04-17 00:30:38 -0500

    Sync index reducer default state with index reducer test

--- src/reducers/index.test.js
+++ src/reducers/index.test.js
@@ -1,13 +1,14 @@
 import index from "./index";
-import { VisibilityFilters } from "../actions";
+import { defaultState as defaultTodosState } from "./todos";
+import { defaultState as defaultVisibilityFilterState } from "./visibilityFilter";
 
 describe(`the index reducer`, () => {
   it(`should return the default state for both`, () => {
     const state = undefined;
     const action = {};
     const expectedOutput = {
-      todos: [],
-      visibilityFilter: VisibilityFilters.SHOW_ALL
+      todos: defaultTodosState,
+      visibilityFilter: defaultVisibilityFilterState
     };
     expect(index(state, action)).toEqual(expectedOutput);
   });
