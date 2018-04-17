ce517c29d8e1ac46c04b1543660ec9765c1075a8
Bill Fienberg <billfienberg@users.noreply.github.com>
2018-04-17 00:23:28 -0500

    Fix visibilityFilter reducer test that started failing after the default state was changed

--- src/reducers/visibilityFilter.test.js
+++ src/reducers/visibilityFilter.test.js
@@ -5,7 +5,7 @@ describe(`the visibilityFilter reducer`, () => {
   it(`should return the default state`, () => {
     const state = undefined;
     const action = {};
-    const expectedOutput = VisibilityFilters.SHOW_COMPLETED;
+    const expectedOutput = VisibilityFilters.SHOW_ALL;
     expect(reducer(state, action)).toEqual(expectedOutput);
   });
 });
